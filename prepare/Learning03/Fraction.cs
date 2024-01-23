8764a9e971b1c0c455cb3d69ed220706a7726eff
Vitor Hugo Santos de Freitas <vitor.hugo.freitas@hotmail.com>
2024-01-23 19:21:18 -0300

    main

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,58 @@
+// Fraction.cs
+
+public class Fraction
+{
+    private int top;
+    private int bottom;
+
+    // Constructors
+    public Fraction()
+    {
+        top = 1;
+        bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        this.top = top;
+        bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        this.top = top;
+        this.bottom = bottom;
+    }
+
+    // Getters and Setters
+    public int GetTop()
+    {
+        return top;
+    }
+
+    public void SetTop(int top)
+    {
+        this.top = top;
+    }
+
+    public int GetBottom()
+    {
+        return bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        this.bottom = bottom;
+    }
+
+    // Methods to return representations
+    public string GetFractionString()
+    {
+        return $"{top}/{bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)top / bottom;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Creating instances of Fraction using different constructors
+        Fraction f1 = new Fraction();            // Default constructor (1/1)
+        Fraction f2 = new Fraction(5);           // Constructor with one parameter (5/1)
+        Fraction f3 = new Fraction(3, 4);        // Constructor with two parameters (3/4)
+        Fraction f4 = new Fraction(1, 3);        // Constructor with two parameters (1/3)
+
+        // Displaying the fraction and decimal representations for each instance
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
     }
 }
